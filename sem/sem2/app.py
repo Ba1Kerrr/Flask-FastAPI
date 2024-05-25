917de6f3d37542f3976cc0d40313ee21922d1742
Denis-Davydov-get <slim2001.88@mail.ru>
2024-05-25 06:26:28 +0300

    add hw2

--- sem/sem2/app.py
+++ sem/sem2/app.py
@@ -3,3 +3,16 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+    return render_template("useruser.html")
+
+
+@app.route("/registration/")
+def registration():
+    return render_template("registration.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
