a630ad2375603a5aab898c3269969c9a2c4b6852
Denis-Davydov-get <slim2001.88@mail.ru>
2024-05-30 22:19:36 +0300

    add del files

---
+++ sem/sem2/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html.html")
+
+
+@app.route("/registration/")
+def registration():
+    return render_template("registration.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
